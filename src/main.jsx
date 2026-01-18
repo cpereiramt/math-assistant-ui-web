8cda8800fd0d0aede61a42a15cdb87d5af1b74f9
Clayton Pereira <cpereiramt@gmail.com>
2026-01-18 16:58:56 -0400

    fix: fixing file

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import { FormulaProvider } from './contexts/FormulaContext.jsx';
 import "./index.css";
 
 if (!import.meta.env.VITE_API_BASE_URL ) {
+  
   throw new Error("VITE_API_BASE_URL not defined");
 }
 
