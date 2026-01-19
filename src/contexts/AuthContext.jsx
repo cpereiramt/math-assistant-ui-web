763fbb9975c14c40dc5c6154043efa2bc8b4daa3
Clayton Pereira <cpereiramt@gmail.com>
2026-01-19 08:29:37 -0400

    fix: fixing hooks configuration

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,30 +1,30 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useCallback, useMemo, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(() => localStorage.getItem("token"));
 
-  const login = (t) => {
+  const login = useCallback((t) => {
     localStorage.setItem("token", t);
     setToken(t);
-  };
+  }, []);
 
-  const logout = () => {
+  const logout = useCallback(() => {
     localStorage.removeItem("token");
     setToken(null);
-  };
+  }, []);
 
-  const redirectToLoginPage = () => {
+  const redirectToLoginPage = useCallback(() => {
     if (!token) {
       // ideal: redirecionar para o endpoint de login do BACKEND (não do front)
       window.location.href = "${API_BASE_URL}/oauth2/authorization/google";
     }
-  };
+  }, [token]);  
 
   const value = useMemo(
     () => ({ token, login, logout, redirectToLoginPage }),
-    [token]
+    [token, login, logout, redirectToLoginPage]
   );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
