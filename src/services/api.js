e3191215c2b3c2be931bd58eafcedc4760a6131a
Clayton Pereira <cpereiramt@gmail.com>
2026-01-19 07:15:31 -0400

    fix: using axios

--- src/services/api.js
+++ src/services/api.js
@@ -2,29 +2,16 @@ import { API_BASE_URL } from "../config/env";
 import { api } from "../utils/axiosConfig";
 const BASE_URL = `${API_BASE_URL}/api/formulas`;
 
-export const fetchFormulas = async (token) => {
-  const res = await api.get(`${BASE_URL}/getAll`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return await res.json();
+export const fetchFormulas = async () => {
+  const res = await api.get(`${BASE_URL}/getAll`);
+  return await res.data;
 };
 
-export const executeFormula = async (name, body, token) => {
-  const res = await api.post(`${BASE_URL}/execute?formulaName=${name}`, body, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return await res.text();
+export const executeFormula = async (name, body) => {
+  const res = await api.post(`${BASE_URL}/execute?formulaName=${name}`, body);
+  return await res.data;
 };
 
-export const deleteFormula = async (id, token) => {
-  await api.delete(`${BASE_URL}/${id}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const deleteFormula = async (id) => {
+  await api.delete(`${BASE_URL}/${id}`);
 };
